83f39bd09e5bae775efa1c877bdfa5e88eef8f38
Peter Grajcar <peter.grajcar131@gmail.com>
2021-10-15 12:22:44 +0200

    finish courseinfo exercises
    Signed-off-by: Peter Grajcar <peter.grajcar131@gmail.com>

---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = ({parts}) => {
+    return (
+        <div>
+            {parts.map(part => <Part part={part} />)}
+        </div>
+    )
+}
+
+export default Content
---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Total = ({parts}) => {
+    const total = parts.reduce((acc, part) => acc + part.exercises, 0)
+    return (
+        <p>Number of exercises {total}</p>
+    )
+}
+
+export default Total;
