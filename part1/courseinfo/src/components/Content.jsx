0e952ae7adfa4f47e53bf1bfd3fd910ac9d113fd
Peter Grajcar <peter.grajcar131@gmail.com>
2021-10-16 15:47:48 +0200

    fix courseinfo exercise
    Signed-off-by: Peter Grajcar <peter.grajcar131@gmail.com>

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -4,7 +4,7 @@ import Part from './Part'
 const Content = ({parts}) => {
     return (
         <div>
-            {parts.map(part => <Part part={part} />)}
+            {parts.map((part, i) => <Part key={i} part={part} />)}
         </div>
     )
 }
